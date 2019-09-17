1eca9d4fcbfbadd3fd55b739cd35112a5ef3148c
Puteulanus <mj@puteulanus.com>
2019-09-17 17:50:00 +0800

    final one

--- week-01/day-02/draw-diamond.js
+++ week-01/day-02/draw-diamond.js
@@ -2,3 +2,30 @@
 
 const lineCount = 7;
 
+function draw_diamond(num) {
+    let arr = []
+    let out = '';
+    let n = num;
+    do {
+        out = out 
+        + ' '.repeat((num-n)/2) 
+        + '*'.repeat(n) 
+        + ' '.repeat((num-n)/2);
+        arr.push(out)
+        out = ''
+    } while (n -= 2 > 0)
+    arr.reverse();
+    arr.forEach(
+        (str) => {
+            console.log(str + "\n");
+        }
+    );
+    arr.reverse();
+    arr.forEach(
+        (str) => {
+            console.log(str + "\n");
+        }
+    );
+}
+
+console.log(draw_diamond(7));
